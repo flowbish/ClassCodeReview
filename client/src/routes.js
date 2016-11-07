0cbf88cc9230097bbaa5ccd8466cd477a8eaeebb
Brandon Chong <bschong2@illinois.edu>
2016-11-07 01:20:57 -0600

    designed login button

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Router, Route } from 'react-router';
 
 import ViewFile from './components/ViewFile';
+import Login from './components/Login';
 import NotFound from './components/NotFound';
 
 const Routes = (props) => (
   <Router {...props}>
     <Route path="/" component={ViewFile} />
+    <Route path="/login" component={Login} />
     <Route path="*" component={NotFound} />
   </Router>
 );
